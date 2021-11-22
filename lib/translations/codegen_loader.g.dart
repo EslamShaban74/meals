0b0a5bd5631fc0887082c71674532a8f093b1989
Eslam <eslamshaban12328@gmail.com>
2021-11-22 14:14:49 +0200

    localization

---
+++ lib/translations/codegen_loader.g.dart
@@ -0,0 +1,112 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> ar = {
+  "title": "الوجبات",
+  "categories": "الأنواع",
+  "favorites": "المفضلة",
+  "filters": "فلاتر",
+  "settings": "الاعدادات",
+  "adjust_your_meal_selection": "قم بتعديل الوجبات التي تظهر لك هنا",
+  "gluten_free": "خالي من الجلوتين",
+  "lactose_free": "خالي من اللاكتوز",
+  "vegetarian": "نباتي",
+  "vegan": "نباتي بدون مشتقات الحيوانات",
+  "only_includes_gluten_free_meals": "اظهر الوجبات الخالية من الجولتين فقط",
+  "only_includes_lactose_free_meals": "اظهر الوجبات الخالية من اللاكتوز فقط",
+  "only_include_vegetarian_meals": "اظهر الوجبات النباتية فقط",
+  "only_include_vegan_meals": "اظهر الوجبات النباتيه فقط بدون مشتقات الحيوانات ",
+  "adjust_your_settings_selection": "قم بتعديل اعداداتك هنا",
+  "choose_your_theme_mode": "قم باختيار شكل التطبيق",
+  "system_default_theme": "شكل الجهاز الافتراضي",
+  "light_theme": "الوضع الفاتح",
+  "dark_theme": "الوضع الداكن",
+  "steps": "الخطوات",
+  "min": "دقيقه",
+  "ingredient": "المكونات",
+  "drawer_title": "فوديا",
+  "favorite_text": "المفضلة فارغه - قم باضافه بعض الوجبات",
+  "cat_c1": "ايطالي",
+  "cat_c2": "وجبات سريعه",
+  "cat_c3": "برجر",
+  "cat_c10": "وجبات صيفية",
+  "cat_c4": "ألماني",
+  "cat_c5": "وجبات خفيفة",
+  "cat_c6": "وجبات غريبة",
+  "cat_c7": "فطار",
+  "cat_c8": "أسيوي",
+  "cat_c9": "فرنسي",
+  "meal_m1": "معكرونه سبجاتي بصلصه الطماطم",
+  "meal_m10": "سلطة الهليون مع الطماطم الكرزية ",
+  "meal_m2": "توست هاواي",
+  "meal_m3": "كلاسيك هامبرجر",
+  "meal_m4": "فينر شميتزل",
+  "meal_m5": "سطلة بالسالمون المدخن",
+  "meal_m6": "موس برتقالي لذيذ",
+  "meal_m7": "فطائر",
+  "meal_m8": "الدجاج الهندي الكريمي ",
+  "meal_m9": "سوفلي الشوكولاته",
+  "choose_your_app_language": "اختر لغة التطبيق"
+};
+static const Map<String,dynamic> en = {
+  "title": "Meal",
+  "categories": "Categories",
+  "favorites": "Favorites",
+  "filters": "Filters",
+  "settings": "Settings",
+  "adjust_your_meal_selection": "Adjust your meal selection here!",
+  "gluten_free": "Gluten-free",
+  "lactose_free": "Lactose-free",
+  "vegetarian": "Vegetarian",
+  "vegan": "Vegan",
+  "only_includes_gluten_free_meals": "only includes gluten free meals",
+  "only_includes_lactose_free_meals": "only includes lactose free meals",
+  "only_include_vegetarian_meals": "only includes vegetarian meals",
+  "only_include_vegan_meals": "only include vegan meals",
+  "adjust_your_settings_selection": "Adjust your settings selection here",
+  "choose_your_theme_mode": "Choose your theme mode",
+  "system_default_theme": "System Default Theme",
+  "light_theme": "Light Theme",
+  "dark_theme": "Dark Theme",
+  "steps": "Steps",
+  "min": "Min",
+  "ingredient": "Ingredients",
+  "drawer_title": "Foodiaa",
+  "favorite_text": "You have no favorites yet - start adding some!",
+  "cat_c1": "Italian",
+  "cat_c2": "Quick &Easy",
+  "cat_c3": "Hamburgers",
+  "cat_c10": "Summer",
+  "cat_c4": "German",
+  "cat_c5": "Light & Lovely",
+  "cat_c6": "Exotic",
+  "cat_c7": "Breakfast",
+  "cat_c8": "Asian",
+  "cat_c9": "French",
+  "meal_m1": "Spaghetti with Tomato Sauce",
+  "meal_m10": "Aspargus Salad with Cherry Tomatoes",
+  "meal_m2": "Toast Hawaii",
+  "meal_m3": "Classic Hamburger",
+  "meal_m4": "Wiener Schmitzel",
+  "meal_m5": "Salad with Smoked Salmon",
+  "meal_m6": "Delicious Orange Mousse",
+  "meal_m7": "Pancakes",
+  "meal_m8": "Creamy Indian Chicken Curry",
+  "meal_m9": "Choclate Souffle",
+  "choose_your_app_language": "Choose your app Language"
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"ar": ar, "en": en};
+}
---
+++ lib/translations/locale_keys.g.dart
@@ -0,0 +1,50 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const title = 'title';
+  static const categories = 'categories';
+  static const favorites = 'favorites';
+  static const filters = 'filters';
+  static const settings = 'settings';
+  static const adjust_your_meal_selection = 'adjust_your_meal_selection';
+  static const gluten_free = 'gluten_free';
+  static const lactose_free = 'lactose_free';
+  static const vegetarian = 'vegetarian';
+  static const vegan = 'vegan';
+  static const only_includes_gluten_free_meals = 'only_includes_gluten_free_meals';
+  static const only_includes_lactose_free_meals = 'only_includes_lactose_free_meals';
+  static const only_include_vegetarian_meals = 'only_include_vegetarian_meals';
+  static const only_include_vegan_meals = 'only_include_vegan_meals';
+  static const adjust_your_settings_selection = 'adjust_your_settings_selection';
+  static const choose_your_theme_mode = 'choose_your_theme_mode';
+  static const system_default_theme = 'system_default_theme';
+  static const light_theme = 'light_theme';
+  static const dark_theme = 'dark_theme';
+  static const steps = 'steps';
+  static const min = 'min';
+  static const ingredient = 'ingredient';
+  static const drawer_title = 'drawer_title';
+  static const favorite_text = 'favorite_text';
+  static const cat_c1 = 'cat_c1';
+  static const cat_c2 = 'cat_c2';
+  static const cat_c3 = 'cat_c3';
+  static const cat_c10 = 'cat_c10';
+  static const cat_c4 = 'cat_c4';
+  static const cat_c5 = 'cat_c5';
+  static const cat_c6 = 'cat_c6';
+  static const cat_c7 = 'cat_c7';
+  static const cat_c8 = 'cat_c8';
+  static const cat_c9 = 'cat_c9';
+  static const meal_m1 = 'meal_m1';
+  static const meal_m10 = 'meal_m10';
+  static const meal_m2 = 'meal_m2';
+  static const meal_m3 = 'meal_m3';
+  static const meal_m4 = 'meal_m4';
+  static const meal_m5 = 'meal_m5';
+  static const meal_m6 = 'meal_m6';
+  static const meal_m7 = 'meal_m7';
+  static const meal_m8 = 'meal_m8';
+  static const meal_m9 = 'meal_m9';
+  static const choose_your_app_language = 'choose_your_app_language';
+
+}
